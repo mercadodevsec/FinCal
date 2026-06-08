424e5a956617118e0b9754add7b2b1e51de8ae40
mercadodevsec <mikkomercado97@gmail.com>
2026-06-08 19:29:33 -0400

    major rename changes

--- app/src/main/java/com/mercadodevsec/fincal/Category3Activity.java
+++ app/src/main/java/com/mercadodevsec/fincal/Category3Activity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FifthOptionActivity extends AppCompatActivity {
+public class Category3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_fifth_option);
+        setContentView(R.layout.activity_category3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/mercadodevsec/fincal/Category5Activity.java
+++ app/src/main/java/com/mercadodevsec/fincal/Category5Activity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FirstOptionActivity extends AppCompatActivity {
+public class Category5Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_first_option);
+        setContentView(R.layout.activity_category5);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
